caa1f3b226bf8f712d27d7fba67680cea0d22548
Bruno Novaes <brunovivo26@gmail.com>
2026-09-26 15:50:43 +0100

    Corrigir player mobile e limpar cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="voz-cura-v4";
+const CACHE="voz-cura-v5";
 const CORE=["./","./index.html","./style.css","./app.js","./self-heal.js","./manifest.webmanifest","./videos.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
