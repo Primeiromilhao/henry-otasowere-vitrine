d5a3ce84930a3a04bf59a2a3d22b899ce52c8df3
Bruno Novaes <brunovivo26@gmail.com>
2026-09-26 20:27:07 +0100

    Atualizar cache do banner aprimorado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="voz-cura-v9";
-const CORE=["./","./index.html","./style.css","./app.js","./self-heal.js","./manifest.webmanifest","./videos.json","./assets/1000254338.jpg"];
+const CACHE="voz-cura-v10";
+const CORE=["./","./index.html","./style.css","./app.js","./self-heal.js","./manifest.webmanifest","./videos.json","./assets/1000254338.jpg","./assets/1000254338-enhanced.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
