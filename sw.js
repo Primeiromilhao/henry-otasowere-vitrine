61560e0dc3197493f35d731f044bfa6447210923
Bruno Novaes <brunovivo26@gmail.com>
2026-09-26 19:59:39 +0100

    Atualizar cache e guardar foto do topo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="voz-cura-v8";
-const CORE=["./","./index.html","./style.css","./app.js","./self-heal.js","./manifest.webmanifest","./videos.json"];
+const CACHE="voz-cura-v9";
+const CORE=["./","./index.html","./style.css","./app.js","./self-heal.js","./manifest.webmanifest","./videos.json","./assets/1000254338.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
